de9dda8d7b699d62674d6c8881bc14748b9c16cc
blondern <niksa.blonder@nist.gov>
2022-07-28 17:26:51 -0400

    moved data categories from data-preservation component to data-categories component

--- oar-dmp/src/app/dmp-api.service.ts
+++ oar-dmp/src/app/dmp-api.service.ts
@@ -8,7 +8,7 @@ export class DmpAPIService {
 
   constructor(private http: HttpClient)  { }
   //URL to get a list of mock contacts from MongoDB using python API
-  API = "http://127.0.0.1:8000/api/people"
+  API = "http://127.0.0.1:8000/people"
 
   public getAll(){
     return this.http.get(this.API);
