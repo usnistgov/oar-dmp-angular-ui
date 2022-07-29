cc128d815037d2125b4b2c6c87f318334fcdd8a1
blondern <niksa.blonder@nist.gov>
2022-07-29 15:16:43 -0400

    added data categories dictionary(map)

--- oar-dmp/src/app/data-categories/data-categories.component.ts
+++ oar-dmp/src/app/data-categories/data-categories.component.ts
@@ -8,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class DataCategoriesComponent implements OnInit {
 
   constructor() { }
+  dataCategories = new Map([
+    ['SRD', false],
+    ['Reference', false],
+    ['Resource', false],
+    ['Published', false],
+    ['Publishable', false],
+    ['Working', false],
+    ['Derived', false]
+  ]);
 
   ngOnInit(): void {
+  }
+
+  dataCategoryChange(e:any) {
+    this.dataCategories.set(e.target.defaultValue,e.target.checked)
+    console.log(this.dataCategories)
+
+    if (e.target.checked){
+      console.log(e.target.defaultValue + " checked");      
+    }
+    else{
+      console.log(e.target.defaultValue + " unchecked");
+    }
   }
 
 }
