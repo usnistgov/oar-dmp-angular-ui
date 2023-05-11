35d44579c0830b6bf1276cbd03925db51ae1adb4
blondern <niksa.blonder@nist.gov>
2023-05-11 18:30:04 -0400

    fixed unit testing errors for keywords component

--- oar-dmp/src/app/form-components/keywords/keywords.component.spec.ts
+++ oar-dmp/src/app/form-components/keywords/keywords.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { KeywordsComponent } from './keywords.component';
-import { DMP_Meta } from 'src/app/types/DMP.types';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
+import { FormBuilder } from '@angular/forms';
 
 
 describe('KeywordsComponent', () => {
@@ -12,7 +12,8 @@ describe('KeywordsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [FormsModule, ReactiveFormsModule],
-      declarations: [ KeywordsComponent ]
+      declarations: [ KeywordsComponent ],
+      providers: [ FormBuilder ]
     })
     .compileComponents();
   });
