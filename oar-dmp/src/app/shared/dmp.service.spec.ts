9579dd5d2c6b65989a1e9cf3c738319161a715dd
blondern <niksa.blonder@nist.gov>
2023-05-17 15:37:15 -0400

    added a new environment for debugging to access local oar-pdr-py API for backend database interraction

--- oar-dmp/src/app/shared/dmp.service.spec.ts
+++ oar-dmp/src/app/shared/dmp.service.spec.ts
@@ -3,10 +3,12 @@ import { TestBed } from '@angular/core/testing';
 import { DmpService } from './dmp.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ConfigurationService } from '../config/config.service';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 describe('DmpService', () => {
   let service: DmpService;
   let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,6 +20,10 @@ describe('DmpService', () => {
   });
 
   it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('new DMP record', () => {
     expect(service).toBeTruthy();
   });
 });
--- oar-dmp/src/environments/environment.ts
+++ oar-dmp/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  configUrl:"assets/environment.json",
+  configUrl:"assets/dev-environment.json",
   debug:true
 };
 
