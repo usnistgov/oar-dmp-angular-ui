5c5ce55fcbab105ad59353cbb53844dd83eba7f6
blondern <niksa.blonder@nist.gov>
2023-06-22 15:39:31 -0400

    added organization mock titles

---
+++ oar-dmp/src/app/types/mock-organizations.ts
@@ -0,0 +1,40 @@
+export interface MockOrganizations {
+  id: number;
+  orgName: string;
+}
+
+export const ORGANIZATIONS: MockOrganizations[] =[
+  {
+    id: 1, 
+    orgName: 'Applied Chemicals and Materials Division',
+  },
+  {
+    id: 2, 
+    orgName: 'Biomolecular Measurement Division',
+  },
+  {
+    id: 3, 
+    orgName: 'Biosystems and Biomaterials Division',
+  },
+  {
+    id: 4, 
+    orgName: 'Chemical Sciences Division',
+  },
+  {
+    id: 5, 
+    orgName: 'Materials Measurement Science Division',
+  },
+  {
+    id: 6, 
+    orgName: 'Materials Science and Engineering Division',
+  },
+  {
+    id: 7, 
+    orgName: 'Office of Data and Informatics',
+  },
+  {
+    id: 8, 
+    orgName: 'Office of Reference Materials',
+  }
+
+]
