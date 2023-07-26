b32db2fcd32a8e6ad05e1508b65f89e0193e1fbb
RayPlante <raymond.plante@nist.gov>
2023-07-26 15:18:05 -0400

    nist-resources.module: bring back code for loading resources automatically

--- oar-dmp/src/app/config/nist-resources.module.ts
+++ oar-dmp/src/app/config/nist-resources.module.ts
@@ -3,11 +3,18 @@ import { HttpClient } from '@angular/common/http';
 
 import { NistResorucesService } from './nist-resoruces.service';
 
+export function configFetcherFactory(configSvc: NistResorucesService) {
+    return () => { 
+        return configSvc.fetchNistResources().toPromise();
+    };
+}
 
 @NgModule({
     providers: [
         HttpClient,
-        NistResorucesService
+        NistResorucesService,
+        { provide: APP_INITIALIZER, useFactory: configFetcherFactory,
+          deps: [ NistResorucesService ], multi: true }
     ]
 })
 export class NistResorucesModule { }
