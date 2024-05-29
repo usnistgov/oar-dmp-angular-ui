cc65c2359830d9c6f060ebf151dca8314fb61daf
Niksa Blonder <niksa.blonder@nist.gov>
2024-05-28 22:32:51 -0400

    commented on pass by reference

--- oar-dmp/src/app/form-components/data-description/data-description.component.ts
+++ oar-dmp/src/app/form-components/data-description/data-description.component.ts
@@ -155,9 +155,9 @@ export class DataDescriptionComponent implements OnInit {
   }
 
   dataCategoryChange(e:any) {
-    this.setStorageTier(e.target.defaultValue,e.target.checked);
+    this.setStorageTier(e.target.defaultValue,e.target.checked);    
     
-    
+    // pass by reference
     let dataCategories = this.dataDescriptionForm.value['dataCategories'] as string[];
 
     if (e.target.checked){      
