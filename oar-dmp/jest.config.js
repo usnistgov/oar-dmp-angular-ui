6ab90e0427c946378954535bc30410efa08a49ea
Niksa Blonder <niksa.blonder@nist.gov>
2026-07-05 15:27:47 -0400

    fixed Jest setup

--- oar-dmp/jest.config.js
+++ oar-dmp/jest.config.js
@@ -1,8 +1,24 @@
 module.exports = {
-    preset: 'jest-preset-angular',
-    setupFilesAfterEnv: ['./setup-jest.ts'],
-    moduleNameMapper: {
-        "^lodash-es$": "lodash"
-    },
-    globalSetup: 'jest-preset-angular/global-setup'
+  preset: 'jest-preset-angular',
+  rootDir: '.',
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^lodash-es$': 'lodash'
+  },
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!.*\\.mjs$)'
+  ]
 };
