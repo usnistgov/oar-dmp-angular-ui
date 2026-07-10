13bb742a76c549277bd264c0b15c3ef69d423462
Niksa Blonder <niksa.blonder@nist.gov>
2026-07-10 12:45:52 -0400

    Trim ResourceOptions to selections present in nist-resources.json
    nist-resources.json only keys highlights off storageSelection and
    softwareSelection, so the database/website tracking was wired but never
    rendered.
    
    - resource-options.component.ts: remove database/website subscriptions,
      fields, and subscribe methods; keep storage + software
    - resource-options.component.html: drop database/website keys from the
      filter binding
    
    Subjects in ResourcesService and the .next() emissions in
    technical-requirements are retained (harmless no-op emissions) to keep
    this change scoped.

--- oar-dmp/src/app/resource-options/resource-options.component.ts
+++ oar-dmp/src/app/resource-options/resource-options.component.ts
@@ -1,11 +1,7 @@
-//Example of passing data from parent component (app.component) to the child component (resource-options.component)
-// Start with declaring Input decorator
-//import { Component, OnInit, Input } from '@angular/core';
-import { Component, OnInit, OnChanges, AfterViewInit} from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 
-
-// In the child, we need to import the service "ResourcesService" file to be able to use it. 
+// In the child, we need to import the service "ResourcesService" file to be able to use it.
 import { ResourcesService } from '../shared/resources.service';
 import { LoadResourcesService } from '../shared/load-resources.service';
 
@@ -19,56 +15,26 @@ export class ResourceOptionsComponent implements OnInit, AfterViewInit {
 
   storageSubscription!: Subscription | null;
   softwareSubscription!: Subscription | null;
-  databaseSubscription!: Subscription | null;
-  websiteSubscription!: Subscription | null;
-  
-
-  // to pass data from parent to this component we declare an input decorator along
-  // with a property named data wich is of type string
-  // @Input() data :any
 
   // we inject shared service ResourcesService in the constructor.
   constructor(
-    private sharedService:ResourcesService, 
-    private nistResources: LoadResourcesService    
-    ) { 
+    private sharedService: ResourcesService,
+    private nistResources: LoadResourcesService
+  ) { }
 
-  }
-
-  // message : any
   storageSelection: string = "";
-  computingSelection: string = "";
-  networkSelection: string = "";
-  
-  websiteSelection: string = "";
-  databaseSelection: string = "";  
-  awsSelection: string = "";
-
-  colaborationSelection: string = "";
   softwareSelection: string = "";
-  dataSelection: string = "";
 
-  availableResources: any = {};   
+  availableResources: any = {};
 
   ngOnInit(): void {
-    // this.message = this.sharedService.getMessage()
     this.storageSubscribe();
     this.softwareSubscribe();
-    this.databaseSubscribe();
-    this.websiteSubscribe();
     this.availableResources = this.nistResources.getAllResources();
-    
-    // console.log(this.availableResources);
   }
+
   ngAfterViewInit(): void {
-    // console.log("resoruce-options after view init");
-    // this.dom.setDomElementTop("resources-grid-container", "dmp_hdr")
-    // this.dom.horizontalDomAdjust("resource_options", "dmp_hdr")
   }
-
-  
-
-  
 
   //subscribe to a particular subject
   storageSubscribe() {
@@ -76,7 +42,7 @@ export class ResourceOptionsComponent implements OnInit, AfterViewInit {
       //subscribe if not already subscribed
       this.storageSubscription = this.sharedService.storageSubject$.subscribe({
         next: (message) => {
-          this.storageSelection = message;          
+          this.storageSelection = message;
         }
       });
     }
@@ -88,29 +54,7 @@ export class ResourceOptionsComponent implements OnInit, AfterViewInit {
       //subscribe if not already subscribed
       this.softwareSubscription = this.sharedService.softwareSubject$.subscribe({
         next: (message) => {
-          this.softwareSelection = message;          
-        }
-      });
-    }
-  }
-
-  databaseSubscribe() {
-    if (!this.databaseSubscription) {
-      //subscribe if not already subscribed
-      this.databaseSubscription = this.sharedService.databaseSubject$.subscribe({
-        next: (message) => {
-          this.databaseSelection = message;
-        }
-      });
-    }
-  }
-
-  websiteSubscribe() {
-    if (!this.websiteSubscription) {
-      //subscribe if not already subscribed
-      this.websiteSubscription = this.sharedService.websiteSubject$.subscribe({
-        next: (message) => {
-          this.websiteSelection = message;
+          this.softwareSelection = message;
         }
       });
     }
